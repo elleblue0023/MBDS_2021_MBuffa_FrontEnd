8b08785c2df037ba011ff75f7ce7037db8fecb8f
elleblue0023 <lovanirinamurelle@gmail.com>
2021-04-12 21:31:09 +0300

    Ready to deploy to heroku

--- assignment-app/server.js
+++ assignment-app/server.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const path = require('path');
+//Install express server
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/assignment-app'));
+app.use(express.static(__dirname + "/dist/assignment-app/"));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/assignment-app/index.html'));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/assignment-app/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(8080);
+app.listen(process.env.PORT || 8081);
